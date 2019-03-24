46a56b6b77bca22d75c42d7dfee480500fd7ec60
Blunde1 <lundeberent@gmail.com>
2019-03-24 22:28:18 +0100

    documentation for data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' The capital/income ratio in Europe, 1870-2010.
+#'
+#' Time series data containing the Private capital / national income ratio for Germany, France and Britain.
+#'
+#' @format A data frame with 15 rows and 4 variables:
+#' \describe{
+#'   \item{Year}{date, only year is accurate}
+#'   \item{Numeric values}{private capital / national income ratio}
+#'   ...
+#' }
+#' @source \url{http://piketty.pse.ens.fr/en/capital21c2}
+"capital_V_income_eu"
